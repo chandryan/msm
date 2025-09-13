dc597f3c54d9b152a9cd887de7b0dfce1f340eb5
Christian Granzin <christian.granzin@gmail.com>
2025-09-13 09:05:47 -0400

    add test for upper fsm

--- test/BackCommon.hpp
+++ test/BackCommon.hpp
@@ -17,11 +17,11 @@
 
 template<typename Front>
 using get_test_machines = boost::mpl::vector<
-    boost::msm::back::state_machine<Front>,
-    boost::msm::back::state_machine<Front, boost::msm::back::favor_compile_time>,
-    boost::msm::backmp11::state_machine<Front>,
-    boost::msm::backmp11::state_machine<Front, boost::msm::backmp11::favor_compile_time>,
-    boost::msm::back11::state_machine<Front>
+    // boost::msm::back::state_machine<Front>,
+    // boost::msm::back::state_machine<Front, boost::msm::back::favor_compile_time>,
+    boost::msm::backmp11::state_machine<Front>
+    // boost::msm::backmp11::state_machine<Front, boost::msm::backmp11::favor_compile_time>,
+    // boost::msm::back11::state_machine<Front>
     >;
 
 template <template <template <typename...> class, typename = void> class hierarchical>
